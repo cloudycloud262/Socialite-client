63e5d1598cfb01609a24e921c59baf0e7eb459e3
cloudycloud262 <138093711+cloudycloud262@users.noreply.github.com>
2023-08-07 01:56:01 +0530

    Delete server directory

--- server/src/routes/chatRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getChats, getMessages } from "../controllers/chatController.js";
-
-const chatRouter = Router();
-
-chatRouter.get("/", getChats);
-chatRouter.get("/:id", getMessages);
-
-export default chatRouter;
